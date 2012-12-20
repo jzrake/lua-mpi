e0a7cf7e2bf45d6ed5243984fbda2b6724398db0
Jonathan Zrake <jonathan.zrake@gmail.com>
2012-12-20 17:31:55 -0500

    Wrap MPI structs by hand, Python script is a mess

--- readspec.py
+++ readspec.py
@@ -3,12 +3,111 @@ import json
 import os
 
 
+mpi_typenames = [
+    'MPI_Aint',
+    'MPI_Comm',
+    'MPI_Comm_copy_attr_function',
+    'MPI_Comm_delete_attr_function',
+    #'MPI_Comm_errhandler_function',
+    'MPI_Datarep_conversion_function',
+    'MPI_Datarep_extent_function',
+    'MPI_Datatype',
+    'MPI_Errhandler',
+    'MPI_File',
+    #'MPI_File_errhandler_function',
+    'MPI_Grequest_cancel_function',
+    'MPI_Grequest_free_function',
+    'MPI_Grequest_query_function',
+    'MPI_Group',
+    'MPI_Info',
+    'MPI_Offset',
+    'MPI_Op',
+    'MPI_Request',
+    'MPI_Status',
+    'MPI_Type_copy_attr_function',
+    'MPI_Type_delete_attr_function',
+    'MPI_User_function',
+    'MPI_Win',
+    'MPI_Win_copy_attr_function',
+    'MPI_Win_delete_attr_function', ]
+    #'MPI_Win_errhandler_function' ]
+
+mpi_datatypes = [
+    'MPI_CHAR',
+    'MPI_BYTE',
+    'MPI_SHORT',
+    'MPI_INT',
+    'MPI_LONG',
+    'MPI_FLOAT',
+    'MPI_DOUBLE',
+    'MPI_UNSIGNED_CHAR',
+    'MPI_UNSIGNED_SHORT',
+    'MPI_UNSIGNED',
+    'MPI_UNSIGNED_LONG',
+    'MPI_LONG_DOUBLE',
+    'MPI_LONG_LONG_INT',
+    'MPI_FLOAT_INT',
+    'MPI_LONG_INT',
+    'MPI_DOUBLE_INT',
+    'MPI_SHORT_INT',
+    'MPI_2INT',
+    'MPI_LONG_DOUBLE_INT',
+    'MPI_PACKED',
+    'MPI_UB',
+    'MPI_LB' ]
+
+mpi_misc = [
+    'MPI_ANY_SOURCE',
+    'MPI_PROC_NULL',
+    'MPI_ROOT',
+    'MPI_ANY_TAG',
+    'MPI_MAX_PROCESSOR_NAME',
+    'MPI_MAX_ERROR_STRING',
+    'MPI_MAX_OBJECT_NAME',
+    'MPI_UNDEFINED',
+    'MPI_CART',
+    'MPI_GRAPH',
+    'MPI_KEYVAL_INVALID',
+]
+
+mpi_skipfuncs = ['MPI_Pcontrol']
+
+mpi_nullobj = {'MPI_Comm': {'NULL': 'MPI_COMM_NULL'},
+               'MPI_Op': {'NULL': 'MPI_OP_NULL'},
+               'MPI_Group': {'NULL': 'MPI_GROUP_NULL'},
+               'MPI_Datatype': {'NULL': 'MPI_DATATYPE_NULL'},
+               'MPI_Request': {'NULL': 'MPI_REQUEST_NULL'},
+               'MPI_Status': {'NULL': 'NULL'},
+               'MPI_Errhandler': {'NULL': 'MPI_ERRHANDLER_NULL'}}
+
+
+def lua_checkarg(dt, vn, num):
+    """ tn: type declaration, vn: variable name, num: arg number """
+    if dt.startswith('MPI_') and dt.endswith('*'):
+        if dt in mpi_typenames: # metatable name ends in *
+            short = dt.replace('MPI_', '')
+        else: # metatable name does not end in *
+            short = dt.replace('MPI_', '').replace('*', '')
+        return """%(dt)s %(vn)s = (%(dt)s) luaL_checkudata(L, %(num)d, "MPI::%(short)s");""" % {
+            'dt': dt, 'vn': vn, 'num': num, 'short': short }
+    elif dt.startswith('MPI_') and not dt.endswith('*'):
+        if dt in mpi_typenames: # metatable name ends in *
+            short = dt.replace('MPI_', '')
+        else: # metatable name does not end in *
+            short = dt.replace('MPI_', '').replace('*', '')
+        return """%(dt)s %(vn)s = *((%(dt)s) luaL_checkudata(L, %(num)d, "MPI::%(short)s"));""" % {
+            'dt': dt, 'vn': vn, 'num': num, 'short': short }
+    else:
+        pass#print "unrecognized:", dt
+        
+
+
 class LuaType(object):
     def __init__(self, name, default=0):
         self.name = name
         self.default = default
     def push_new(self):
-        funcs = (
+        noptr_funcs = (
 """
 static void luampi_push_%(mname)s(lua_State *L, %(name)s arg)
 {
@@ -21,9 +120,30 @@ static int _%(mname)s(lua_State *L)
   return 1;
 }""" % {'name': self.name,
         'sname': self.name.replace('MPI_', ''),
+        'mname': self.name,
+        'default': self.default})
+
+        ptr_funcs = (
+"""
+static void luampi_push_%(mname)s(lua_State *L, %(name)s arg, int N)
+{
+  memcpy(lua_newuserdata(L, N*sizeof(%(name)s)), arg, N*sizeof(%(name)s));
+  luaL_setmetatable(L, "MPI::%(sname)s");
+}
+static int _%(mname)s(lua_State *L)
+{
+  int N = lua_checkinteger(L, 1);
+  luampi_push_%(mname)s(L, %(default)s, N);
+  return 1;
+}""" % {'name': self.name,
+        'sname': self.name.replace('MPI_', ''),
         'mname': self.name.replace('*', '_ptr'),
         'default': self.default})
-        print funcs,
+
+        if '*' in self.name:
+            return ptr_funcs
+        else:
+            return noptr_funcs
 
     def newmetatable(self):
         print """  luaL_newmetatable(L, "MPI::%s"); lua_pop(L, 1);""" % self.name[4:]
@@ -41,6 +161,13 @@ class LuaFunction(object):
         self.ret_type = spec['retVal']
 
     def write(self):
+        arg_list = [ ]
+        arg_num = 1
+        for t, n in zip(self.arg_types, self.arg_names):
+            #ca = lua_checkarg(t, n, arg_num)
+            #if ca: print ca
+            arg_num += 1
+
         func = (
             """
 static int h5lua_%(func_name)s(lua_State *L)
@@ -56,51 +183,30 @@ static int h5lua_%(func_name)s(lua_State *L)
          'ret_statement': '' })
         print func,
 
-
-type_names = [
-    'MPI_Aint',
-    'MPI_Comm',
-    'MPI_Comm_copy_attr_function*',
-    'MPI_Comm_delete_attr_function*',
-    #'MPI_Comm_errhandler_function*',
-    'MPI_Datarep_conversion_function*',
-    'MPI_Datarep_extent_function*',
-    'MPI_Datatype',
-    'MPI_Errhandler',
-    'MPI_File',
-    #'MPI_File_errhandler_function*',
-    'MPI_Grequest_cancel_function*',
-    'MPI_Grequest_free_function*',
-    'MPI_Grequest_query_function*',
-    'MPI_Group',
-    'MPI_Info',
-    'MPI_Offset',
-    'MPI_Op',
-    'MPI_Request',
-    'MPI_Status*',
-    'MPI_Type_copy_attr_function*',
-    'MPI_Type_delete_attr_function*',
-    'MPI_User_function*',
-    'MPI_Win',
-    'MPI_Win_copy_attr_function*',
-    'MPI_Win_delete_attr_function*', ]
-    #'MPI_Win_errhandler_function*' ]
+      
 
 
-descr = {'MPI_Comm': {'NULL': 'MPI_COMM_NULL'},
-         'MPI_Op': {'NULL': 'MPI_OP_NULL'},
-         'MPI_Group': {'NULL': 'MPI_GROUP_NULL'},
-         'MPI_Datatype': {'NULL': 'MPI_DATATYPE_NULL'},
-         'MPI_Request': {'NULL': 'MPI_REQUEST_NULL'},
-         'MPI_Errhandler': {'NULL': 'MPI_ERRHANDLER_NULL'}}
+
+all_types = set()
+
+luampi_funcs = [ ]
+for spec in os.listdir('specs'):
+    func = LuaFunction(json.load(open('specs/' + spec)))
+    if func.func_name in mpi_skipfuncs: continue
+    luampi_funcs.append(func)
+    for tname in func.arg_types:
+        all_types.add(tname)
 
 
-for tname in type_names:
-    lua_type = LuaType(tname, default=descr.get(tname, {'NULL':0})['NULL'])
-    #lua_type.newmetatable()
-    lua_type.push_new()
-    #lua_type.setfuncs_list()
+#for func in luampi_funcs:
+    #func.write()
 
 
-#for line in open('specs/constants.txt'):
-#    print line.strip()+';\n',
+#print '\n'.join(sorted(all_types))
+
+
+for tname in mpi_typenames:
+    if 'function' in tname: print """ "%s", """ % tname
+    #lua_type = LuaType(tname)
+    #print lua_type.push_new()
+
--- run.lua
+++ run.lua
@@ -9,10 +9,31 @@ MPI.Init()
 MPI.Comm_rank(MPI.COMM_WORLD, rank)
 MPI.Comm_size(MPI.COMM_WORLD, size)
 
-local comm = MPI.MPI_Comm()
+local callback_types = {
+   "Comm_copy_attr_function",
+   "Comm_delete_attr_function",
+   "Datarep_conversion_function", 
+   "Datarep_extent_function",
+   "Grequest_cancel_function",
+   "Grequest_free_function",
+   "Grequest_query_function",
+   "Type_copy_attr_function",
+   "Type_delete_attr_function",
+   "User_function",
+   "Win_copy_attr_function",
+   "Win_delete_attr_function",
+}
+for _,v in ipairs(callback_types) do
+   local f = MPI[v]()
+   local mt = debug.getregistry()["MPI::"..v]
+   assert(debug.getmetatable(f) == mt)
+end
+
+local comm = MPI.Comm()
 assert(type(comm) == 'userdata')
 
-print(buffer.get_typed(rank, buffer.int, 0))
-print(buffer.get_typed(size, buffer.int, 0))
+assert(buffer.get_typed(rank, buffer.int, 0) == 0)
+assert(buffer.get_typed(size, buffer.int, 0) == 1)
 
 MPI.Finalize()
+print(debug.getinfo(1).source, ": All tests passed")
